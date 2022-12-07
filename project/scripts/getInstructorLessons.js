6e58e062dd00b7d23580744f4263f4cb426fc479
Samuel Larsson <samuel@familjenlarsson.eu>
2022-12-07 14:33:15 +0100

    Update instructor-lessons scripts with minLessons argument

--- project/scripts/getInstructorLessons.js
+++ project/scripts/getInstructorLessons.js
@@ -1,9 +1,11 @@
-const [_node, _name, unparsedYearMonth] = process.argv;
+const [_node, _name, unparsedYearMonth, unparsedMinLessons] = process.argv;
 const yearMonth =
 	unparsedYearMonth &&
 	/^\d{4}-(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)$/.exec(unparsedYearMonth)
 		? unparsedYearMonth
 		: '2021-OCT';
+const minLessons =
+	unparsedMinLessons && /^\d+$/.exec(unparsedMinLessons) ? Number(unparsedMinLessons) : 1;
 
 console.log(`
 	SELECT
@@ -18,7 +20,7 @@ console.log(`
 	INNER JOIN lesson
 	ON lesson.id = booked_lessons.lesson
 	GROUP BY lesson.instructor
-	HAVING COUNT(*) > 1
+	HAVING COUNT(*) > ${minLessons}
 	ORDER BY COUNT(*) DESC
 ;
 `);
